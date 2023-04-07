2387814b1bcfc626e6a7a18707d7855e794bb9fc
junhseo-42seoul <junhseo@student.42seoul.kr>
2023-04-07 17:40:19 +0900

    it will print pointer memory with 0x

--- printf/print_lst.c
+++ printf/print_lst.c
@@ -6,7 +6,7 @@
 /*   By: junhseo <junhseo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 17:24:11 by junhseo           #+#    #+#             */
-/*   Updated: 2023/04/07 17:35:06 by junhseo          ###   ########.fr       */
+/*   Updated: 2023/04/07 17:39:05 by junhseo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,11 @@ void	print_lst(const char type_spec, va_list *lst, int *count)
 	else if (type_spec == 's')
 		put_str(va_arg(*lst, char *), count);
 	else if (type_spec == 'p')
+	{
+		write(1, "0x", 2);
+		*count += 1;
 		put_sx(va_arg(*lst, unsigned int), count);
+	}
 	else if (type_spec == 'd' || type_spec == 'i')
 		put_nbr(va_arg(*lst, int), count);
 	else if (type_spec == 'u')
