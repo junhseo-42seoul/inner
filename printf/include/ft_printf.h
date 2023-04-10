16140169e9f47751a18ee907e4c3456a9fbb38ce
junhseo-42seoul <junhseo@student.42seoul.kr>
2023-04-10 19:04:36 +0900

    remove put_u

--- printf/include/ft_printf.h
+++ printf/include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: junhseo <junhseo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 14:54:26 by junhseo           #+#    #+#             */
-/*   Updated: 2023/04/10 15:53:52 by junhseo          ###   ########.fr       */
+/*   Updated: 2023/04/10 19:03:57 by junhseo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int		ft_printf(const char *str, ...);
 int		is_type(const char *type_specifier, const char str);
 void	put_sx(unsigned long long n, int *count);
 void	put_lx(unsigned long long n, int *count);
-void	put_u(unsigned int n, int *count);
 void	put_per(int *count);
 void	put_mem(void *ptr, int *count);
 void	print_lst(const char type_spec, va_list *lst, int *count);
