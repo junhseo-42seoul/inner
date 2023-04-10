6ad938208d2dd1997a61bcf1c2c3e48775580f0c
junhseo-42seoul <junhseo@student.42seoul.kr>
2023-04-10 09:58:59 +0900

    'x' have ull, put_mem call put_sx

--- printf/put_lx.c
+++ printf/put_lx.c
@@ -6,13 +6,13 @@
 /*   By: junhseo <junhseo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 16:37:44 by junhseo           #+#    #+#             */
-/*   Updated: 2023/04/07 19:04:34 by junhseo          ###   ########.fr       */
+/*   Updated: 2023/04/10 09:58:28 by junhseo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/ft_printf.h"
 
-void	put_lx(unsigned int n, int *count)
+void	put_lx(unsigned long long n, int *count)
 {
 	char	*base;
 	char	tmp;
--- printf/put_mem.c
+++ printf/put_mem.c
@@ -6,7 +6,7 @@
 /*   By: junhseo <junhseo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 18:38:49 by junhseo           #+#    #+#             */
-/*   Updated: 2023/04/10 09:52:35 by junhseo          ###   ########.fr       */
+/*   Updated: 2023/04/10 09:58:23 by junhseo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	put_mem(void *ptr, int *count)
 {
-	unsigned long long mem;
+	unsigned long long	mem;
 
 	mem = (unsigned long long)ptr;
 	write(1, "0x", 2);
