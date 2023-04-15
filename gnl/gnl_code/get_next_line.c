7d8aa94f2083e5d514f2472d9fba199a55d6cbc6
junhseo-42seoul <junhseo@student.42seoul.kr>
2023-04-15 14:25:43 +0900

    for paco

--- gnl/gnl_code/get_next_line.c
+++ gnl/gnl_code/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: junhseo <junhseo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 09:52:33 by junhseo           #+#    #+#             */
-/*   Updated: 2023/04/15 13:33:42 by junhseo          ###   ########.fr       */
+/*   Updated: 2023/04/15 14:07:18 by junhseo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,31 +23,4 @@ char	*get_next_line(int fd)
 	while (*(bbuff + index) != '\n' && *(bbuff + index) != '\0')
 		index++;
 	return (decision_rv(fd, &bbuff, index));
-}
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-int main()
-{
-	int fd;
-	char *str;
-
-	char path1[] = "example.txt";
-	fd = open(path1, O_RDWR);
-	str = get_next_line(fd);
-	printf("%s", str);
-	free(str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	free(str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	free(str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	free(str);
-	close(fd);
 }
